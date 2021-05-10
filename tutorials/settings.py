138e59a542cb7e853b68691b5f9c5aa3c129c5fa
Aaron <Aaron9034@Hotmail.com>
2021-05-10 12:02:37 -0500

    more changes

--- tutorials/settings.py
+++ tutorials/settings.py
@@ -1,34 +1,41 @@
 # includes matplotlib settings and small functions to
 # make the other notebooks look a little shorter
 
+def matplotsettings():
+    ## Plotting Parameters For matplotlib
+    def figsize(scale):
+        fig_width_pt = 513.17 #469.755    # Get this from LaTeX using \the\textwidth
+        inches_per_pt = 1.0 / 72.27         # Convert pt to inch
+        golden_mean = (np.sqrt(5.0)-1.0)/2.0    # Aesthetic ratio
+        fig_width = fig_width_pt * inches_per_pt * scale  # width in inches
+        fig_height = fig_width * golden_mean              # height in inches
+        fig_size = [fig_width, fig_height]
+        return fig_size
 
-def trim_set(parfiles, timfiles):
-    """
-    Trim full pulsar set down to those timed >6 years.
+    plt.rcParams.update(plt.rcParamsDefault)
+    params = {'backend': 'pdf',
+            'axes.labelsize': 10,
+            'lines.markersize': 4,
+            'font.size': 10,
+            'xtick.major.size':6,
+            'xtick.minor.size':3,
+            'ytick.major.size':6,
+            'ytick.minor.size':3,
+            'xtick.major.width':0.5,
+            'ytick.major.width':0.5,
+            'xtick.minor.width':0.5,
+            'ytick.minor.width':0.5,
+            'lines.markeredgewidth':1,
+            'axes.linewidth':1.2,
+            'legend.fontsize': 7,
+            'xtick.labelsize': 10,
+            'ytick.labelsize': 10,
+            'savefig.dpi':200,
+            'path.simplify':True,
+            'font.family': 'serif',
+            'font.serif':'Times',
+            'text.latex.preamble': [r'\usepackage{amsmath}'],
+            'text.usetex':True,
+            'figure.figsize': figsize(0.5)}
 
-    Use this if you want sampling to go a little faster. The results will be different
-    than the full pulsar list.
-
-    Inputs:
-        parfiles (list): list of all .par file location strings
-        timfiles (list): list of all .tim file location strings
-
-    Outputs:
-        parfiles_cut (list): list of .par files for subset of pulsars
-        timfiles_cut (list): list of .tim files for subset of pulsars
-    """
-    psr_names = ['B1855+09', 'B1937+21', 'J0030+0451', 'J0613-0200',
-                 'J0645+5158', 'J1012+5307', 'J1024-0719', 'J1455-3330',
-                 'J1600-3053', 'J1614-2230', 'J1640+2224', 'J1643-1224',
-                 'J1713+0747', 'J1738+0333', 'J1744-1134', 'J1903+0327',
-                 'J1909-3744', 'J1910+1256', 'J1918-0642', 'J1944+0907',
-                 'J2010-1323', 'J2145-0750', 'J2317+1439']
-    parfiles_cut = []
-    timfiles_cut = []
-    for par in parfiles:
-        if par.split('/')[-1].split('_')[0] in psr_names:
-            parfiles_cut.append(par)
-    for tim in timfiles:
-        if tim.split('/')[-1].split('_')[0] in psr_names:
-            timfiles_cut.append(tim)
-    return parfiles_cut, timfiles_cut
+    plt.rcParams.update(params)
